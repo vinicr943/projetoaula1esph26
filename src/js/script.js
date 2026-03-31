a1338ae903f09568740b370fcbd99dc826d0d260
vinicius <vinicr3009@gmail.com>
2026-03-31 11:08:51 -0300

    feat: exercicio operadores

--- src/js/script.js
+++ src/js/script.js
@@ -16,53 +16,65 @@
 // taxaJuros =0.50;
 // console.log(taxaJuros);
 
-var exemplo1="Ola dev var";
-console.log(exemplo1);
+// var exemplo1="Ola dev var";
+// console.log(exemplo1);
 
-let exemplo2="Ola dev let";
-console.log(exemplo2);
+// let exemplo2="Ola dev let";
+// console.log(exemplo2);
 
-const exemplo3="ola dev const";
-console.log(typeof exemplo3);
+// const exemplo3="ola dev const";
+// console.log(typeof exemplo3);
 
-let exemplo4=20;
-console.log(typeof exemplo4);
+// let exemplo4=20;
+// console.log(typeof exemplo4);
 
-let exemplo5=true;
-console.log(typeof exemplo5);
+// let exemplo5=true;
+// console.log(typeof exemplo5);
 
-let exemplo6={nome:"fiap"};
-console.log(exemplo6);
+// let exemplo6={nome:"fiap"};
+// console.log(exemplo6);
 
-let exemplo7=["Java","Python","C#"];
-console.log(typeof exemplo7);
-//variavel indefinida
-let exemplo8;
-console.log(exemplo8);
-//variavel vazia
-let exemplo9=null;
-console.log(exemplo9)
+// let exemplo7=["Java","Python","C#"];
+// console.log(typeof exemplo7);
+// //variavel indefinida
+// let exemplo8;
+// console.log(exemplo8);
+// //variavel vazia
+// let exemplo9=null;
+// console.log(exemplo9)
 
 
-const valor1=20;
-const valor2=10;
+// const valor1=20;
+// const valor2=10;
 
-console.log(valor1 + valor2)
-console.log(valor1 / valor2)
-console.log(valor1 - valor2)
-console.log(valor1 * valor2)
+// console.log(valor1 + valor2)
+// console.log(valor1 / valor2)
+// console.log(valor1 - valor2)
+// console.log(valor1 * valor2)
 
-//operadores logicos
-const valor1=20
-console.log(valor1 > valor2);
-console.log(valor >10 && valor2 !=20)
-console.log(valor2 < 5 || valor1 > 3)
+// //operadores logicos
 
-//operadores de comparacao
-console.log(valor1 < valor2)
-console.log(valor1 >= valor2)
-console.log(valor1 <= valor2)
-console.log(valor1 != valor2)
-console.log(valor1 == valor2)
-console.log(valor1 === valor2)
-console.log("o valor é:", valor2)
+// console.log(valor1 > valor2);
+// console.log(valor1 >10 && valor2 !=20)
+// console.log(valor2 < 5 || valor1 > 3)
+
+// //operadores de comparacao
+// console.log(valor1 < valor2)
+// console.log(valor1 >= valor2)
+// console.log(valor1 <= valor2)
+// console.log(valor1 != valor2)
+// console.log(valor1 == valor2)
+// console.log(valor1 === valor2)
+// console.log("o valor é:", valor2)
+
+const p=20;
+const v=10;
+const precoFinal = p-v;
+console.log("o preço final com desconto é:",precoFinal)
+
+const usuariologado=1;
+const token=100;
+const transacaoautorizada= usuariologado === 1 && token >=10;
+console.log("transaçao autorizada com sucesso", transacaoautorizada)
+
+
