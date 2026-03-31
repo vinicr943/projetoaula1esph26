5fd106180f553052d6f37b9a94685bdb51cd2032
vinicius <vinicr3009@gmail.com>
2026-03-31 10:45:05 -0300

    feat: operadores aritmeticos, logicos e comparaçao

---
+++ src/js/script.js
@@ -0,0 +1,68 @@
+// Declarações e Variaveis
+//var,let e const
+// var xpto=10;
+// console.log(xpto);
+// var xpto=20;
+// console.log(xpto);
+//var - pode redeclarar e acessar fora do escopo
+//let não pode redeclara e não consegue acessar fora do escopo
+// if(true){
+//     var nome="Fiap";
+// }
+//   console.log(nome);
+
+//const
+// const taxaJuros =0.85;
+// taxaJuros =0.50;
+// console.log(taxaJuros);
+
+var exemplo1="Ola dev var";
+console.log(exemplo1);
+
+let exemplo2="Ola dev let";
+console.log(exemplo2);
+
+const exemplo3="ola dev const";
+console.log(typeof exemplo3);
+
+let exemplo4=20;
+console.log(typeof exemplo4);
+
+let exemplo5=true;
+console.log(typeof exemplo5);
+
+let exemplo6={nome:"fiap"};
+console.log(exemplo6);
+
+let exemplo7=["Java","Python","C#"];
+console.log(typeof exemplo7);
+//variavel indefinida
+let exemplo8;
+console.log(exemplo8);
+//variavel vazia
+let exemplo9=null;
+console.log(exemplo9)
+
+
+const valor1=20;
+const valor2=10;
+
+console.log(valor1 + valor2)
+console.log(valor1 / valor2)
+console.log(valor1 - valor2)
+console.log(valor1 * valor2)
+
+//operadores logicos
+const valor1=20
+console.log(valor1 > valor2);
+console.log(valor >10 && valor2 !=20)
+console.log(valor2 < 5 || valor1 > 3)
+
+//operadores de comparacao
+console.log(valor1 < valor2)
+console.log(valor1 >= valor2)
+console.log(valor1 <= valor2)
+console.log(valor1 != valor2)
+console.log(valor1 == valor2)
+console.log(valor1 === valor2)
+console.log("o valor é:", valor2)
